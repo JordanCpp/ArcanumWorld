ad683e5b09b84c491326e01293bd625e9b56c972
Евгений <jordan631@yandex.ru>
2023-05-08 22:46:34 +0400

    Deleted the extra code

--- source/Game/Engine.cpp
+++ source/Game/Engine.cpp
@@ -2,39 +2,12 @@
 #include <LDL/Enums/KeyboardKey.hpp>
 #include <LDL/OpenGL/OpenGL1_2.hpp>
 
-using namespace Arcanum::Game;
 using namespace LDL::Graphics;
 using namespace LDL::Allocators;
 using namespace LDL::Enums;
-using namespace LDL::Math;
 using namespace LDL::Loaders;
-
-GLuint LoadTexture(const std::string& path, ImageLoader& loader)
-{
-	GLuint result = 0;
-
-	glGenTextures(1, &result);
-	glBindTexture(GL_TEXTURE_2D, result);
-	// set the texture wrapping parameters
-	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
-	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
-	// set texture filtering parameters
-	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
-	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
-	// load image, create texture and generate mipmaps
-	loader.Load(path);
-
-	GLenum format = 0;
-
-	if (loader.BytesPerPixel() == 4)
-		format = GL_RGBA;
-	else
-		format = GL_RGB;
-
-	glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, (GLsizei)loader.Size().PosX(), (GLsizei)loader.Size().PosY(), 0, format, GL_UNSIGNED_BYTE, loader.Pixels());
-
-	return result;
-}
+using namespace LDL::Events;
+using namespace Arcanum::Game;
 
 Engine::Engine(Settings* settings) :
 	_Settings(settings),
@@ -48,8 +21,7 @@ Engine::Engine(Settings* settings) :
 	_SecReader(&_ByteReader),
 	_LocationPainter(&_Render),
 	_WidgetManager(&_Render),
-	_GameMenu(&_Render),
-	_LocationPainter3D(&_TileData)
+	_GameMenu(&_Render)
 {
 	_SecReader.Reset(_PathManager.Path("maps/Tarant Sewers-01/", "0.sec"));
 
@@ -63,18 +35,12 @@ Engine::Engine(Settings* settings) :
 	}
 	
 	_Location.Sceneries()[_Location.Index(3, 7)].Init(_SpriteManager.Scenery("savanna_tree02.ART"));
-
 	_Location.Sceneries()[_Location.Index(9, 7)].Init(_SpriteManager.Scenery("engine.ART"));
-
-
-	_TileData.Reset(Point2u(10, 10));
-
-	//_Tex1 = LoadTexture("tr256.bmp", _ImageLoader);
 }
 
 void Engine::Run()
 {
-	LDL::Events::Event report = { 0 };
+	Event report = { 0 };
 
 	size_t dx   = 0;
 	size_t dy   = 0;
@@ -85,37 +51,15 @@ void Engine::Run()
 		_FpsLimiter.Mark();
 		_FpsCounter.Start();
 
-		if (report.Type == LDL::Events::IsQuit)
+		if (report.Type == IsQuit)
 		{
 			_Window.StopEvent();
 		}
-
-		//_Camera.Handle(report);
-
-		//glViewport(0, 0, (GLsizei)_Render.Size().PosX(), (GLsizei)_Render.Size().PosY());
 
 		_Render.Begin();
 
 		_Render.Color(Color(0, 0, 0));
 		_Render.Clear();
-
-		/*
-		glClearColor(0, 0, 0, 1.0f);
-		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-
-		glMatrixMode(GL_PROJECTION);
-		glLoadMatrixf(_Camera.Projection().Values());
-
-		glMatrixMode(GL_MODELVIEW);
-		glLoadMatrixf(_Camera.ModelView().Values());
-		glEnable(GL_TEXTURE_2D);
-
-		glBindTexture(_Tex1, GL_TEXTURE_2D);
-
-		_LocationPainter3D.Draw();
-
-		glDisable(GL_TEXTURE_2D);
-		*/
 
 		_LocationPainter.Draw(&_Location, Point2u(dx, dy));
 
